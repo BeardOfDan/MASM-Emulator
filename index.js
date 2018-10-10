251d763854119634ec4d6f7253f9aa44d13a56e7
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-10 13:50:45 -0700

    (feat) Create index.html

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 5000;
 const publicPath = path.join(__dirname, './public/');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res, next) => {
-  res.send('Home Page');
-});
+// app.get('/', (req, res, next) => {
+//   res.send('Home Page');
+// });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
